b1462c5231d629f3bf162f3265fe2bd3a166ad8e
SudoBefehl <dobroliam30@gmail.com>
2026-08-24 14:00:12 +0200

    Tunnel-URL automatisch aktualisiert: https://appeals-solving-libs-applicant.trycloudflare.com

--- js/config.js
+++ js/config.js
@@ -7,4 +7,4 @@
  * des Skripts Ã¼berschrieben.
  */
 
-window.UBUNTU_HOSTING_API_BASE = "https://deutsch-chronicles-refer-insight.trycloudflare.com";
+window.UBUNTU_HOSTING_API_BASE = "https://appeals-solving-libs-applicant.trycloudflare.com";
