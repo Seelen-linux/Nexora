e684f17e281b87f038f28aa2d3e2c5ecfb8e8916
SudoBefehl <dobroliam30@gmail.com>
2026-08-24 13:44:10 +0200

    Tunnel-URL automatisch aktualisiert: https://handbags-gods-fabrics-week.trycloudflare.com

--- js/config.js
+++ js/config.js
@@ -7,4 +7,4 @@
  * des Skripts Ã¼berschrieben.
  */
 
-window.UBUNTU_HOSTING_API_BASE = "https://route-estimate-compromise-cooper.trycloudflare.com";
+window.UBUNTU_HOSTING_API_BASE = "https://handbags-gods-fabrics-week.trycloudflare.com";
