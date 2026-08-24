f97049fa49e5fa22b4b539ed94276e1b5ccd1329
SudoBefehl <dobroliam30@gmail.com>
2026-08-24 17:13:23 +0200

    Tunnel-URL automatisch aktualisiert: https://endless-cad-soa-duck.trycloudflare.com

--- js/config.js
+++ js/config.js
@@ -7,4 +7,4 @@
  * des Skripts Ã¼berschrieben.
  */
 
-window.UBUNTU_HOSTING_API_BASE = "https://buddy-although-affordable-biol.trycloudflare.com";
+window.UBUNTU_HOSTING_API_BASE = "https://endless-cad-soa-duck.trycloudflare.com";
