e6a209d70e75addcb78d9e81c1abbb03104f3baf
SudoBefehl <dobroliam30@gmail.com>
2026-08-25 05:47:01 +0200

    Tunnel-URL automatisch aktualisiert: https://bond-lightbox-westminster-tier.trycloudflare.com

--- js/config.js
+++ js/config.js
@@ -7,4 +7,4 @@
  * des Skripts Ã¼berschrieben.
  */
 
-window.UBUNTU_HOSTING_API_BASE = "https://statement-poll-carter-alphabetical.trycloudflare.com";
+window.UBUNTU_HOSTING_API_BASE = "https://bond-lightbox-westminster-tier.trycloudflare.com";
