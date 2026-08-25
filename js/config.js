642c49009054cf7c489b16daa59a22bf0a99eff1
SudoBefehl <dobroliam30@gmail.com>
2026-08-25 13:02:21 +0200

    Tunnel-URL automatisch aktualisiert: https://superior-tested-armstrong-bags.trycloudflare.com

--- js/config.js
+++ js/config.js
@@ -7,4 +7,4 @@
  * des Skripts Ã¼berschrieben.
  */
 
-window.UBUNTU_HOSTING_API_BASE = "https://sys-hats-velvet-mode.trycloudflare.com";
+window.UBUNTU_HOSTING_API_BASE = "https://superior-tested-armstrong-bags.trycloudflare.com";
