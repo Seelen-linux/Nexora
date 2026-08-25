30752cc92faa8018a8f09ed68a3290295c4252cb
SudoBefehl <dobroliam30@gmail.com>
2026-08-25 04:18:25 +0200

    Tunnel-URL automatisch aktualisiert: https://displaying-brilliant-chassis-alex.trycloudflare.com

--- js/config.js
+++ js/config.js
@@ -7,4 +7,4 @@
  * des Skripts Ã¼berschrieben.
  */
 
-window.UBUNTU_HOSTING_API_BASE = "https://impacts-def-chrome-cricket.trycloudflare.com";
+window.UBUNTU_HOSTING_API_BASE = "https://displaying-brilliant-chassis-alex.trycloudflare.com";
