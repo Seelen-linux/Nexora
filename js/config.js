def05ad07bdd972fc5d99e72f826a820bd087865
SudoBefehl <dobroliam30@gmail.com>
2026-08-25 13:13:35 +0200

    Tunnel-URL automatisch aktualisiert: https://maui-assumed-subsidiary-effectively.trycloudflare.com

--- js/config.js
+++ js/config.js
@@ -7,4 +7,4 @@
  * des Skripts Ã¼berschrieben.
  */
 
-window.UBUNTU_HOSTING_API_BASE = "https://superior-tested-armstrong-bags.trycloudflare.com";
+window.UBUNTU_HOSTING_API_BASE = "https://maui-assumed-subsidiary-effectively.trycloudflare.com";
