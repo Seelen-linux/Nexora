f0ec7e1c55e77c8064df1b60f322bc7baac4abb9
SudoBefehl <dobroliam30@gmail.com>
2026-08-25 03:49:25 +0200

    Tunnel-URL automatisch aktualisiert: https://sponsorship-henderson-ash-pest.trycloudflare.com

--- js/config.js
+++ js/config.js
@@ -7,4 +7,4 @@
  * des Skripts Ã¼berschrieben.
  */
 
-window.UBUNTU_HOSTING_API_BASE = "https://sandwich-prep-strengthen-wolf.trycloudflare.com";
+window.UBUNTU_HOSTING_API_BASE = "https://sponsorship-henderson-ash-pest.trycloudflare.com";
