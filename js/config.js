db16552e06d34d1d8c9ef30637f4b006c40e8469
SudoBefehl <dobroliam30@gmail.com>
2026-08-25 05:36:05 +0200

    Tunnel-URL automatisch aktualisiert: https://gym-cindy-fig-wanna.trycloudflare.com

--- js/config.js
+++ js/config.js
@@ -7,4 +7,4 @@
  * des Skripts Ã¼berschrieben.
  */
 
-window.UBUNTU_HOSTING_API_BASE = "https://mitchell-incurred-strongly-internship.trycloudflare.com";
+window.UBUNTU_HOSTING_API_BASE = "https://gym-cindy-fig-wanna.trycloudflare.com";
